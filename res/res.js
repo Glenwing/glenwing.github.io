99f4188c5f164f21d490467088a8a0557c3a94fd
Glenwing <11250765+Glenwing@users.noreply.github.com>
2021-01-21 01:50:53 -0800

    Resolution test 2

--- res/res.js
+++ res/res.js
@@ -6,10 +6,13 @@ function updateRes() {
     var pxRatio = window.devicePixelRatio;
     var trueWidth = winWidth * pxRatio;
     var trueHeight = winHeight * pxRatio;
+    var availWidth = window.screen.availWidth;
+    var availHeight = window.screen.availHeight;
 
     $('#RESULT_TRUERES').html(trueWidth + '&#x202f;&times;&#x202f;' + trueHeight);
     $('#RESULT_SCALING').html(LongDivide(pxRatio, 1, LD_5sf));
     $('#RESULT_EFFRES').html(winWidth + '&#x202f;&times;&#x202f;' + winHeight);
+    $('#RESULT_AVAILRES').html(availWidth + '&#x202f;&times;&#x202f;' + availHeight);
 }
 
 updateRes();
