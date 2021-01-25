d6c037d58aa744dce8a679aab8a60a47ecf2aa7b
Glenwing <11250765+Glenwing@users.noreply.github.com>
2021-01-24 23:19:56 -0800

    Res browser compatibility 10

--- res/resDetect.js
+++ res/resDetect.js
@@ -9,26 +9,26 @@ function resDetect() {
     if (UA.indexOf('like gecko') > 0 && UA.indexOf('chrome') > 0) {
         // Chrome / Opera / Blink
         DEBUG('Browser rendering engine: Blink (Chrome, Opera)');
-        engine = 'blink';
+        engine = 'Blink';
     }
     else if (UA.indexOf('like gecko') > 0 && UA.indexOf('applewebkit') > 0) {
         // WebKit
         DEBUG('Browser rendering engine: WebKit (Safari)');
-        engine = 'webkit';
+        engine = 'WebKit';
     }
     else if (UA.indexOf('gecko') > 0) {
         // Gecko / Firefox
         DEBUG('Browser rendering engine: Gecko (Firefox)');
-        engine = 'gecko';
+        engine = 'Gecko';
     }
     else {
-        engine = 'unknown';
+        engine = 'Unknown';
     }
 
     // Get viewport scale to compensate for zoom on mobile devices
     var metaContent = document.querySelector('meta[name="viewport"]').content;
     //console.log('metaContent =', metaContent);
-    var viewportScale = parseNum(
+    var viewportScale = Decimal(parseNum(
         metaContent.substring(
             metaContent.indexOf('initial-scale=') + ('initial-scale=').length,
             metaContent.length
@@ -39,46 +39,52 @@ function resDetect() {
                 metaContent.length
             ).indexOf(',')
         )
-    );
-    if (isNum(viewportScale) == true) {
+    ));
+    if (isNum(viewportScale) == true && window.innerHeight > window.outerHeight) {
         viewportScale = Decimal(viewportScale);
     }
     else {
-        viewportScale = 1;
+        viewportScale = Decimal(1);
     }
     //console.log('viewportScale =', viewportScale);
 
     // Determine output
-    var zoom = 1; // Browser Zoom
-    var zoom_raw = 1; // Browser Zoom (no rounding)
-    var scale = 1; // OS Scale
-    var scaleToUse = 1; // Which scale factor to use in calculating the resolution based on the window.screen.width
-    if (engine == 'gecko') {
+    var zoom = Decimal(1); // Browser Zoom
+    var zoom_raw = Decimal(1); // Browser Zoom (no rounding)
+    var osScale = Decimal(1); // OS Scale
+    var resScale = Decimal(1); // Which scale factor to use in calculating the resolution based on the window.screen.width
+    if (engine == 'Gecko') {
         zoom_raw = Decimal(mediaQueryBinarySearch('min-resolution', 'dppx', 0, 10, 20, 0.0001));
         zoom = zoom_raw.toDecimalPlaces(2);
-        scale = pxRatio.div(zoom).toDecimalPlaces(2);
-        scaleToUse = pxRatio;
+        osScale = pxRatio.div(zoom).toDecimalPlaces(2);
+        resScale = pxRatio;
     }
-    else if (engine == 'blink') {
-        zoom_raw = Decimal(window.outerWidth).div(window.innerWidth).div(viewportScale);
+    else if (engine == 'Blink') {
+        if (window.outerWidth == window.innerWidth + 16) {
+            // This condition indicates 100% zoom but with a non-maximized window
+            zoom_raw = Decimal(1);
+        }
+        else {
+            zoom_raw = Decimal(window.outerWidth).div(window.innerWidth).div(viewportScale);
+        }
         zoom = zoom_raw;
-        scale = pxRatio.div(zoom).toDecimalPlaces(2);
-        scaleToUse = scale;
+        osScale = pxRatio.div(zoom).toDecimalPlaces(2);
+        resScale = osScale;
     }
-    else if (engine == 'webkit') {
+    else if (engine == 'WebKit') {
         // Placeholder for now
-        zoom = 1;
-        scale = pxRatio;
-        scaleToUse = scale;
+        zoom = Decimal(1);
+        osScale = pxRatio;
+        resScale = pxRatio;
     }
 
     return {
         'pxRatio': pxRatio,
         'zoomRaw': zoom_raw,
         'zoomScale': zoom,
-        'osScale': scale,
+        'osScale': osScale,
         'viewportScale': viewportScale,
-        'resScale': scaleToUse,
+        'resScale': resScale,
         'engine': engine,
     }
 }
